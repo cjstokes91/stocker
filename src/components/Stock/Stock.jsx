1f503046878d28b77d9f0ea96dbc3ae118d85627
carl stokes <carlstokes@carls-MBP.lan>
2020-06-02 13:48:17 -0700

    stock refactor

--- src/components/Stock/Stock.jsx
+++ src/components/Stock/Stock.jsx
@@ -20,14 +20,21 @@ export const stock = {
         return formattedData
     },
     getYesterdaysClose: (ticker, date, callback) => {
-        fetch(stock.yesterdaysCloseURL(ticker, date))
-            .then((response) => response.json())
-            .then((data) => callback(stock.formatPriceData(data)))
-
+        stock.getLastTradingDate(date).then((data) => {
+            console.log(data.date)
+            fetch(stock.yesterdaysCloseURL(ticker, date))
+                .then((res) => res.json())
+                .then((data) => callback(stock.formatPriceData(data)))
+        })
+    },
+    getLastTradingDate: (date) => {
+        let today = new
+            Date(date).toISOString().split('T')[0].replace(/-/g, '');
+        const url = `${iex.base_url}/ref-data/us/dates/trade/last/1/${today}?token=${iex.api_token}`
+        return fetch(url).then((res) => res.json());
     },
 
     yesterdaysCloseURL: (ticker, date) => {
         return `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&exactDate=20200601&token=${iex.api_token}`;
     }
-
 }
